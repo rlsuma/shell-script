3506b871237a85329c87a2a90107fee988d307d5
umamahesh <vumamaheshwararaov@gmail.com>
2024-04-25 16:03:50 +0530

    hello script

---
+++ 10-functions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+USERID(id -u)
+VALIDATE(){
+    echo "Exit status: $1"
+    echo "What are you doing :$2"
+
+}
+if[$USERID -ne 0]
+then
+  echo "please run this script with root access."
+  exit 1
+  else 
+  echo "you are super user."
+
+  dnf install mysql -y
+  VALIDATE $? "Installing MYSQL"
+  
+  dnf insatll git -y
+  VALIDATE $? "Installing git"

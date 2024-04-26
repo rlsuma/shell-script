ab2a273168a699917b14b3927ea1ad0b2a878137
umamahesh <vumamaheshwararaov@gmail.com>
2024-04-26 13:45:40 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,14 +2,13 @@
 
 USERID=$(id -u)
 
-#VALIDATE(){
-  if [$1 -ne 0]; then 
-  
-  echo "$2.....FAILURE"
-    exit 1
-        else 
-    echo "$2.....SUCCeSS"
-    
+VALIDATE(){
+    if [ $1 -ne 0 ]; 
+    then 
+        echo "$2.....FAILURE"
+        exit 1
+    else 
+       echo "$2.....SUCCeSS"
     fi
 }
 

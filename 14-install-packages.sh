c54a5c8e6acc24466e31316de2335c50ae863285
umamahesh <vumamaheshwararaov@gmail.com>
2024-04-26 16:56:03 +0530

    functions

---
+++ 14-install-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+   echo "please run this script with root access."
+   exit 1
+   else 
+     echo "your are super user."
+     fi
+
+     echo "all packages: $@"
+     

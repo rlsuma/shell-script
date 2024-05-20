3434f4a36e57c85c9f36dd3abe02e458fb3b774a
umamahesh <vumamaheshwararaov@gmail.com>
2024-05-20 15:04:17 +0500

    backend

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -4,5 +4,7 @@ echo "Brfore calling other script course: $COURSE"
 echo "process ID of current shell script : $$"
 
 #./16-other-script.sh
+
 source ./16-other-script.sh
+
 echo "After calling other script,course: $COURSE"

725f2cb33de9198f04427c7e7f2349e7e8c353a4
umamahesh <vumamaheshwararaov@gmail.com>
2024-05-20 14:19:15 +0500

    backend

---
+++ 15-current-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+COURSE="devops from current script "
+echo "Brfore calling other script course: $COURSE"
+echo "process ID of current shell script : $$"
+
+#./16-other-script.sh
+echo "After calling other script,course: $COURSE"
